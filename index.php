f66c425d7aad006717cd9cf5f3a4e259f5a709c7
Tanapat Limsombat <temmer.se@gmail.com>
2014-05-06 14:01:52 +0700

    insert By Temmer

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT By Temmer
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
